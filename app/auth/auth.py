df436d374883de7f57e04be6b5500f7399ff9eea
idalz <idalez@outlook.com>
2025-04-22 00:22:58 +0300

    Add .env

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -1,7 +1,11 @@
 from datetime import datetime, timedelta, timezone
 from jose import JWTError, jwt
+import os
+from dotenv import load_dotenv
 
-SECRET_KEY = "gonnabesupersecretkey"
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
--- app/database.py
+++ app/database.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
+from dotenv import load_dotenv
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:2486@localhost/chat_db")
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
