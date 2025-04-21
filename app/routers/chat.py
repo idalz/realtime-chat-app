56d8782e104732e505e436ad6abe210d46376a95
idalz <idalez@outlook.com>
2025-04-21 19:18:53 +0300

    Create and connect with database.

--- app/routers/chat.py
+++ app/routers/chat.py
@@ -1,15 +1,23 @@
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+from sqlalchemy.orm import Session
+from app.database import get_db
 from app.websocket_manager import ConnectionManager
+from app.models.message import Message
 
 router = APIRouter()
 manager = ConnectionManager()
 
 @router.websocket("/ws/{username}")
 async def websocket_endpoint(websocket: WebSocket, username: str):
+    db: Session = next(get_db())
     await manager.connect(username, websocket)
     try:
         while True:
             data = await websocket.receive_text()
+
+            msg = Message(sender=username, content=data)
+            db.add(msg)
+            db.commit()
             await manager.broadcast(data, sender=username)
     except WebSocketDisconnect:
         manager.disconnect(username)
