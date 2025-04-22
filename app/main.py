c5b2d60b772fcc94b3eecf30a334c47ef3044c05
idalz <idalez@outlook.com>
2025-04-22 19:01:20 +0300

    Add init front end

--- app/main.py
+++ app/main.py
@@ -3,6 +3,10 @@ from contextlib import asynccontextmanager
 from app.routers import chat, auth
 from app.redis_listener import redis_subscriber
 import asyncio
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi.requests import Request
 
 app = FastAPI()
 
@@ -13,5 +17,22 @@ async def lifespan(app: FastAPI):
     task.cancel()
 
 app = FastAPI(lifespan=lifespan)
+
 app.include_router(chat.router)
 app.include_router(auth.router)
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+templates = Jinja2Templates(directory="app/templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def get_chat_page(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/login", response_class=HTMLResponse)
+async def get_login_page(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+@app.get("/signup", response_class=HTMLResponse)
+async def get_register_page(request: Request):
+    return templates.TemplateResponse("signup.html", {"request": request})
